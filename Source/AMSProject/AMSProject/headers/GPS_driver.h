c3833aa6d058eecf0120d757959f551e3c0d0b94
lsangild <lsangild@gmail.com>
2018-04-11 23:25:20 +0200

    Added an idea for UBX communication with GPS

--- Source/AMSProject/AMSProject/headers/GPS_driver.h
+++ Source/AMSProject/AMSProject/headers/GPS_driver.h
@@ -9,11 +9,14 @@
 #ifndef GPS_DRIVER_H_
 #define GPS_DRIVER_H_
 
+#include <string.h>
 #include "uart_driver.h"
 
 extern struct uart_t gpsUart;
 
 void InitGPS();
+
+const char* ConstructGPSMessage(uint8_t class, uint8_t ID, int payload);
 
 
 #endif /* GPS_DRIVER_H_ */
--- Source/AMSProject/AMSProject/source/GPS_driver.c
+++ Source/AMSProject/AMSProject/source/GPS_driver.c
@@ -6,3 +6,34 @@
  */ 
 
 #include "GPS_driver.h"
+
+const char* ConstructUBXMessage(uint8_t class, uint8_t ID, int length, char * payload) {
+    // Setup
+    uint8_t CK_A = 0;
+    uint8_t CK_B = 0;
+    // Init forst to zero to ensure all array is zero
+    uint8_t checksum_length = 6 + length + 2;
+    char buffer[checksum_length] = {0};
+    buffer[2] = class;
+    buffer[3] = ID;
+    
+    // Fill in payload
+    for (int i = 4, i < (4 + length), i++) {
+        output[i] = &payload[i - 4];
+    }
+    
+    // Calculate checksum
+    for (int i = 0, i < checksum_length, i++) {
+        CK_A = CK_A + &payload[i];
+        CK_B = CK_B + CK_A;
+    }
+    
+    output[6 + length] = CK_A;
+    output[6 + length + 1] = CK_B;
+    
+    // Add Sync Chars (Done later to not include into checksum)
+    buffer[0] = 0xB5;
+    buffer[1] = 0x62;
+    
+    return *buffer;
+}
