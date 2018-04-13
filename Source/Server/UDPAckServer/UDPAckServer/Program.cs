c5e7644086ad786b4b04b19d03a66ce576ca4aaf
CperPedersen <Boye82@hotmail.com>
2018-04-13 13:00:38 +0200

    Added Respond server.

---
+++ Source/Server/UDPAckServer/UDPAckServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPAckServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient listener = new UdpClient(25565);
+            byte[] data = new byte[1024]; 
+
+            while (true)
+            {
+                IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+                data = listener.Receive(ref sender);
+
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+
+                string msg = "Ack + " + data.Length;
+                byte[] msgData = Encoding.ASCII.GetBytes(msg);
+
+                listener.Send(msgData, msgData.Length, sender);
+            }            
+        }
+    }
+}
