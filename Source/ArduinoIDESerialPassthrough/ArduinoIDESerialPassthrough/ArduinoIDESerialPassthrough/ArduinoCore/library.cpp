239ced75eccb04267b390cb29905b670a0e468e3
lsangild <lsangild@gmail.com>
2018-05-16 21:52:09 +0200

    Added Working 9600 baud Atmel project

---
+++ Source/ArduinoIDESerialPassthrough/ArduinoIDESerialPassthrough/ArduinoIDESerialPassthrough/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 16/05/2018 17.56.17
+ * Author : au338486
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
